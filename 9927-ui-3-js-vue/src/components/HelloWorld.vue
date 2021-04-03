a9bd4d79818a551d3c4881869caae98d78667309
PavelK6896 <79208776896@yandex.ru>
2021-04-03 22:08:03 +0300

    add ui vue

---
+++ 9927-ui-3-js-vue/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>
+    {{ info }}
+  </p>
+  <button @click="getUser">
+    getUser
+  </button>
+
+</template>
+
+<script>
+const url1 = 'http://localhost:8081'
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      info: null
+    }
+  },
+  methods: {
+    getUser() {
+      fetch(url1).then((d) => d.json()).then(response => (this.info = response))
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
